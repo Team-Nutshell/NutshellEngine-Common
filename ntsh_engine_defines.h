4385aaf0633530e7644ef4af82aad7da92ce3d0c
ZaOniRinku <zaonirinku@gmail.com>
2022-09-28 20:05:31 +0200

    exit on assert

--- ntsh_engine_defines.h
+++ ntsh_engine_defines.h
@@ -9,7 +9,8 @@
 #define NTSH_ASSERT(expression) \
 	do { \
 		if (!expression) { \
-			std::cerr << "\33[1m\33[31mASSERTION FAILED: " << #expression << " failed on line " << __LINE__ << " in file " << __FILE__ << "." << std::endl; \
+			std::cerr << "\33[1m\33[31mASSERTION FAILED: " << #expression << ".\nLine " << __LINE__ << " in file " << __FILE__ << "." << std::endl; \
+			exit(1); \
 		} \
 	} while(0)
 #else
