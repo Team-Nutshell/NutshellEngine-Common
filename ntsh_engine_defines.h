ffcda94e706dfdb206fb35da106673f12509ebbe
ntsh-oni <teamnutshell.dev@gmail.com>
2022-09-29 12:07:42 +0200

    fix the assert expression

--- ntsh_engine_defines.h
+++ ntsh_engine_defines.h
@@ -9,7 +9,7 @@
 #ifndef NDEBUG
 #define NTSH_ASSERT(expression) \
 	do { \
-		if (!expression) { \
+		if (!(expression)) { \
 			std::cerr << "\33[1m\33[31mASSERTION FAILED\33[39m\33[0m: " << #expression << ".\nLine \33[1m" << __LINE__ << "\33[0m in file \33[1m" << __FILE__ << "\33[0m." << std::endl; \
 			exit(1); \
 		} \
