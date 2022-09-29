ee458798520f315ab71786cde35c1c10591aa0c3
ntsh-oni <teamnutshell.dev@gmail.com>
2022-09-29 13:41:01 +0200

    add the Module interface

---
+++ ntsh_module_interface.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "ecs.h"
+#include <string>
+
+class NutshellModuleInterface : public System {
+protected:
+    std::string m_name = "";
+public:
+    NutshellModuleInterface() {}
+    virtual ~NutshellModuleInterface() {}
+
+    virtual void init() = 0;
+    virtual void update(double dt) = 0;
+    virtual void destroy() = 0;
+
+    const std::string& getName() const {
+        return m_name;
+    }
+};
+
+typedef NutshellModuleInterface* createModule();
+typedef void destroyModule(NutshellModuleInterface*);
