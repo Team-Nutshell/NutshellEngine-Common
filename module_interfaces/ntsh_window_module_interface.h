e3101c2ab1c7a737d8e6a8d651195bce3042dbad
ZaOniRinku <zaonirinku@gmail.com>
2022-10-01 13:56:34 +0200

    add functions to the window module interface

--- module_interfaces/ntsh_window_module_interface.h
+++ module_interfaces/ntsh_window_module_interface.h
@@ -11,6 +11,23 @@ public:
     virtual void init() = 0;
     virtual void update(double dt) = 0;
     virtual void destroy() = 0;
+
+    // Send a request to close the window
+    virtual void closeWindow() = 0;
+    // Returns true if there has been a request to close the window, else if there has been no request to close the window
+    virtual bool shouldClose() = 0;
+
+    // Changes the size of the window to width and height
+    virtual void setWindowSize(int width, int height) = 0;
+    // Returns the width of the window
+    virtual int getWindowWidth() = 0;
+    // Returns the height of the window
+    virtual int getWindowHeight() = 0;
+
+    // Returns true if the window is in fullscreen mode, else, return false
+    virtual bool isFullscreen() = 0;
+    // If the fullscreen parameter is true, puts the window in fullscreen, else, puts the window in windowed
+    virtual void setFullscreen(bool fullscreen) = 0;
 };
 
 #ifdef NTSH_OS_WINDOWS
