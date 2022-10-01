678e5b13cfa620834ee82660ea48d1d620de77a6
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-01 16:00:51 +0200

    replaces spaces for tabs

--- module_interfaces/ntsh_graphics_module_interface.h
+++ module_interfaces/ntsh_graphics_module_interface.h
@@ -4,13 +4,13 @@
 class NutshellGraphicsModuleInterface : public NutshellModuleInterface {
 protected:
 public:
-    NutshellGraphicsModuleInterface() {}
-    NutshellGraphicsModuleInterface(const std::string& name) : NutshellModuleInterface(NTSH_MODULE_GRAPHICS, name) {}
-    virtual ~NutshellGraphicsModuleInterface() {}
+	NutshellGraphicsModuleInterface() {}
+	NutshellGraphicsModuleInterface(const std::string& name) : NutshellModuleInterface(NTSH_MODULE_GRAPHICS, name) {}
+	virtual ~NutshellGraphicsModuleInterface() {}
 
-    virtual void init() = 0;
-    virtual void update(double dt) = 0;
-    virtual void destroy() = 0;
+	virtual void init() = 0;
+	virtual void update(double dt) = 0;
+	virtual void destroy() = 0;
 };
 
 #ifdef NTSH_OS_WINDOWS
--- module_interfaces/ntsh_module_interface.h
+++ module_interfaces/ntsh_module_interface.h
@@ -4,22 +4,22 @@
 
 class NutshellModuleInterface : public System {
 protected:
-    NtshModuleType m_type = NTSH_MODULE_UNKNOWN;
-    std::string m_name = "";
+	NtshModuleType m_type = NTSH_MODULE_UNKNOWN;
+	std::string m_name = "";
 public:
-    NutshellModuleInterface() {}
-    NutshellModuleInterface(const NtshModuleType type, const std::string& name) : m_type(type), m_name(name) {}
-    virtual ~NutshellModuleInterface() {}
+	NutshellModuleInterface() {}
+	NutshellModuleInterface(const NtshModuleType type, const std::string& name) : m_type(type), m_name(name) {}
+	virtual ~NutshellModuleInterface() {}
 
-    virtual void init() = 0;
-    virtual void update(double dt) = 0;
-    virtual void destroy() = 0;
+	virtual void init() = 0;
+	virtual void update(double dt) = 0;
+	virtual void destroy() = 0;
 
-    const NtshModuleType getType() const {
-        return m_type;
-    }
+	const NtshModuleType getType() const {
+		return m_type;
+	}
 
-    const std::string& getName() const {
-        return m_name;
-    }
+	const std::string& getName() const {
+		return m_name;
+	}
 };
