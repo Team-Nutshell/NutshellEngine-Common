c0c64682fc4f09fd480cff0c8f65f1a23513b2eb
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-07 16:31:19 +0200

    remove function definitions in module interfaces

--- module_interfaces/ntsh_audio_module_interface.h
+++ module_interfaces/ntsh_audio_module_interface.h
@@ -12,11 +12,3 @@ public:
 	virtual void update(double dt) = 0;
 	virtual void destroy() = 0;
 };
-
-#ifdef NTSH_OS_WINDOWS
-typedef NutshellAudioModuleInterface* (__stdcall *createAudioModule_t)();
-typedef void (__stdcall *destroyAudioModule_t)(NutshellAudioModuleInterface*);
-#elif NTSH_OS_LINUX
-typedef NutshellAudioModuleInterface* createAudioModule_t();
-typedef void destroyAudioModule_t(NutshellAudioModuleInterface*);
-#endif
