d2862716b4871097380901f9deffd8c1a44ccfdb
ZaOniRinku <zaonirinku@gmail.com>
2022-10-14 21:20:26 +0200

    remove cpp file and static identifier

--- utils/ntsh_file_utils.h
+++ utils/ntsh_file_utils.h
@@ -1,12 +1,51 @@
 #pragma once
+#include <fstream>
 #include <string>
 
 namespace utils {
 	namespace file {
-		static std::string readAscii(const std::string& filePath);
-		static std::string readBinary(const std::string& filePath);
-		static std::string filename(const std::string& filePath);
-		static std::string extension(const std::string& filePath);
-		static bool exists (const std::string& filePath);
+		std::string readAscii(const std::string& filePath) {
+			std::ifstream file(filePath, std::ios::in | std::ios::ate);
+			if (!file.is_open()) {
+				return "";
+			}
+			file.seekg(0);
+			std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+			return fileContent;
+		}
+
+		std::string readBinary(const std::string& filePath) {
+			std::ifstream file(filePath, std::ios::in | std::ios::ate | std::ios::binary);
+			if (!file.is_open()) {
+				return "";
+			}
+			file.seekg(0);
+			std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+			return fileContent;
+		}
+
+		std::string filename(const std::string& filePath) {
+			size_t slashPosition = filePath.find_last_of('/');
+			if (slashPosition == std::string::npos) {
+				slashPosition = filePath.find_last_of('\\');
+				if (slashPosition == std::string::npos) {
+					return filePath;
+				}
+			}
+			return filePath.substr(slashPosition + 1);
+		}
+
+		std::string extension(const std::string& filePath) {
+			size_t dotPosition = filePath.find_last_of('.');
+			if (dotPosition == std::string::npos) {
+				return "";
+			}
+			return filePath.substr(dotPosition + 1);
+		}
+
+		bool exists (const std::string& filePath) {
+			std::ifstream file(filePath);
+			return file.good();
+		}
 	}
 }
