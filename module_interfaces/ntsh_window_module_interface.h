50a26efca0682affbc6613774672d25082985b10
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-16 16:40:50 +0200

    add a function to signal when the window got resized

--- module_interfaces/ntsh_window_module_interface.h
+++ module_interfaces/ntsh_window_module_interface.h
@@ -34,6 +34,9 @@ public:
 	// If the fullscreen parameter is true, puts the window in fullscreen, else, puts the window in windowed
 	virtual void setFullscreen(bool fullscreen) = 0;
 
+	// Returns true if the window just got resized
+	virtual bool gotResized() = 0;
+
 	// Sets the title of the window
 	virtual void setTitle(const std::string& title) = 0;
 
