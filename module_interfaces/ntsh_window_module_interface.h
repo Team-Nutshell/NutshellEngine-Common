48134e51f754ac5c01ef96f663cd698a57dc3131
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-16 17:39:58 +0200

    remove the gotResize function and add the pollEvents function

--- module_interfaces/ntsh_window_module_interface.h
+++ module_interfaces/ntsh_window_module_interface.h
@@ -18,33 +18,33 @@ public:
 	virtual void destroy() = 0;
 
 	// Send a request to close the window
-	virtual void closeWindow() = 0;
+	virtual void close() = 0;
 	// Returns true if there has been a request to close the window, else if there has been no request to close the window
 	virtual bool shouldClose() = 0;
 
 	// Changes the size of the window to width and height
-	virtual void setWindowSize(int width, int height) = 0;
+	virtual void setSize(int width, int height) = 0;
 	// Returns the width of the window
-	virtual int getWindowWidth() = 0;
+	virtual int getWidth() = 0;
 	// Returns the height of the window
-	virtual int getWindowHeight() = 0;
+	virtual int getHeight() = 0;
 
 	// Returns true if the window is in fullscreen mode, else, return false
 	virtual bool isFullscreen() = 0;
 	// If the fullscreen parameter is true, puts the window in fullscreen, else, puts the window in windowed
 	virtual void setFullscreen(bool fullscreen) = 0;
 
-	// Returns true if the window just got resized
-	virtual bool gotResized() = 0;
+	// Poll events
+	virtual bool pollEvents() = 0;
 
 	// Sets the title of the window
 	virtual void setTitle(const std::string& title) = 0;
 
 #ifdef NTSH_OS_WINDOWS
 	// Returns the native Win32 window handle
-	virtual HWND getWindowHandle() = 0;
+	virtual HWND getNativeHandle() = 0;
 #elif NTSH_OS_LINUX
 	// Returns the native X window handle
-	virtual Window getWindowHandle() = 0;
+	virtual Window getNativeHandle() = 0;
 #endif
 };
