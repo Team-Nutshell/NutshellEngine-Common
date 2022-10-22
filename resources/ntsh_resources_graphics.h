a5768963c84607660c1f7f37c53da5b287b35956
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-22 21:05:12 +0200

    split resources file in multiple files

---
+++ resources/ntsh_resources_graphics.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <cstdint>
+
+// Image
+typedef enum NtshImageFormat {
+	NTSH_IMAGE_FORMAT_R8,
+	NTSH_IMAGE_FORMAT_R8G8,
+	NTSH_IMAGE_FORMAT_R8G8B8,
+	NTSH_IMAGE_FORMAT_R8G8B8A8,
+	NTSH_IMAGE_FORMAT_R16,
+	NTSH_IMAGE_FORMAT_R16G16,
+	NTSH_IMAGE_FORMAT_R16G16B16,
+	NTSH_IMAGE_FORMAT_R16G16B16A16,
+	NTSH_IMAGE_FORMAT_R32,
+	NTSH_IMAGE_FORMAT_R32G32,
+	NTSH_IMAGE_FORMAT_R32G32B32,
+	NTSH_IMAGE_FORMAT_R32G32B32A32
+} NtshImageFormat;
+
+struct NtshImage {
+	// Image width
+	uint32_t width;
+	
+	// Image height
+	uint32_t height;
+
+	// Image format
+	NtshImageFormat format;
+
+	// Data, size can be calculated from width, height and format
+	uint8_t* pData;
+};
