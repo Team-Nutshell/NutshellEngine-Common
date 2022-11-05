9cc8982da7f74218b1b8d93bb39f1949e3f12ab4
ntsh-oni <teamnutshell.dev@gmail.com>
2022-11-05 11:01:31 +0100

    add models to the graphics resources

--- resources/ntsh_resources_graphics.h
+++ resources/ntsh_resources_graphics.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <vector>
+#include <utility>
 
 // Image
 typedef enum NtshImageFormat {
@@ -15,19 +16,74 @@ typedef enum NtshImageFormat {
 	NTSH_IMAGE_FORMAT_R32,
 	NTSH_IMAGE_FORMAT_R32G32,
 	NTSH_IMAGE_FORMAT_R32G32B32,
-	NTSH_IMAGE_FORMAT_R32G32B32A32
+	NTSH_IMAGE_FORMAT_R32G32B32A32,
+	NTSH_IMAGE_FORMAT_UNKNOWN
 } NtshImageFormat;
 
 struct NtshImage {
 	// Image width
-	uint32_t width;
+	uint32_t width = 0;
 	
 	// Image height
-	uint32_t height;
+	uint32_t height = 0;
 
 	// Image format
-	NtshImageFormat format;
+	NtshImageFormat format = NTSH_IMAGE_FORMAT_UNKNOWN;
 
 	// Data
 	std::vector<uint8_t> data;
+};
+
+// Material
+struct NtshMaterial {
+	// Diffuse (base color) texture
+	NtshImage* diffuseTexture = nullptr;
+
+	// Normal texture
+	NtshImage* normalTexture = nullptr;
+
+	// Metalness, Roughness and Occlusion textures can be the same one
+	// In that case, R = Occlusion, G = Roughness, B = Metalness
+	// Metalness texture
+	NtshImage* metalnessTexture = nullptr;
+
+	// Roughness texture
+	NtshImage* roughnessTexture = nullptr;
+	
+	// Occlusion texture
+	NtshImage* occlusionTexture = nullptr;
+	
+	// Emissive texture
+	NtshImage* emissiveTexture = nullptr;
+};
+
+// Vertex
+struct NtshVertex {
+	float position[3] = { 0.0f, 0.0f, 0.0f };
+	float normal[3] = { 0.0f, 0.0f, 0.0f };
+	float uv[2] = { 0.0f, 0.0f };
+	float color[3] = { 0.0f, 0.0f, 0.0f };
+	float tangent[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+	float joints[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+	float weights[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+};
+
+// Mesh
+typedef enum NtshTopology {
+	NTSH_TOPOLOGY_TRIANGLE_LIST,
+	NTSH_TOPOLOGY_TRIANGLE_STRIP,
+	NTSH_TOPOLOGY_LINE_LIST,
+	NTSH_TOPOLOGY_LINE_STRIP,
+	NTSH_TOPOLOGY_POINT_LIST
+} NtshTopology;
+
+struct NtshMesh {
+	std::vector<NtshVertex> vertices;
+	std::vector<uint32_t> indices;
+	NtshTopology topology = NTSH_TOPOLOGY_TRIANGLE_LIST;
+};
+
+// Model
+struct NtshModel {
+	std::vector<std::pair<NtshMesh, NtshMaterial>> primitives;
 };
