af54f0642deb049617cffee86db01999c197db45
ZaOniRinku <zaonirinku@gmail.com>
2022-12-12 15:55:24 +0100

    exit with an int value

--- utils/ntsh_engine_defines.h
+++ utils/ntsh_engine_defines.h
@@ -12,7 +12,7 @@
 	do { \
 		if (!(expression)) { \
 			std::cerr << "\33[1m\33[31mASSERTION FAILED\33[39m\33[0m: " << #expression << ".\nLine \33[1m" << __LINE__ << "\33[0m in file \33[1m" << __FILE__ << "\33[0m." << std::endl; \
-			exit(NTSH_RESULT_ASSERT_FAILED); \
+			exit(1); \
 		} \
 	} while(0)
 #else
