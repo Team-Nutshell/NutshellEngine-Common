fb78fd12181e041ec430f33f38e4cb50a852feaa
ZaOniRinku <zaonirinku@gmail.com>
2022-12-13 09:27:22 +0100

    add input support to the window module interface

---
+++ utils/ntsh_engine_mouse_enums.h
@@ -0,0 +1,13 @@
+#pragma once
+
+enum class NtshInputMouseButton {
+	Any, // Any mouse button
+	One, // Left mouse button
+	Two, // Right mouse button
+	Three, // Middle mouse button
+	Four, // Additional mouse button 4
+	Five, // Additional mouse button 5
+	Six, // Additional mouse button 6
+	Seven, // Additional mouse button 7
+	Eight // Additional mouse button 8
+};
