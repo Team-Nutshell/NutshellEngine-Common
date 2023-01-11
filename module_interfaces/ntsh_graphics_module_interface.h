5a7a286c867961eca91773d4ff4f93911b4080e4
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-11 14:41:24 +0100

    add mesh and image loading functions to the graphics module interface

--- module_interfaces/ntsh_graphics_module_interface.h
+++ module_interfaces/ntsh_graphics_module_interface.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "ntsh_module_interface.h"
+#include "../resources/ntsh_resources_graphics.h"
+
+typedef uint64_t NtshMeshId;
+typedef uint64_t NtshImageId;
 
 class NutshellGraphicsModuleInterface : public NutshellModuleInterface {
 public:
@@ -10,4 +14,9 @@ public:
 	virtual void init() = 0;
 	virtual void update(double dt) = 0;
 	virtual void destroy() = 0;
+
+	// Loads the mesh described in the mesh parameter in the internal format and returns a unique identifier
+	virtual NtshMeshId load(const NtshMesh mesh) = 0;
+	// Loads the image described in the image parameter in the internal format and returns a unique identifier
+	virtual NtshImageId load(const NtshImage image) = 0;
 };
