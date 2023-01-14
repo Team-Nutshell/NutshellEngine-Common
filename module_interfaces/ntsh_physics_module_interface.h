b13538227e4f290a356e70f9e3314d7d6952e2a7
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-14 10:43:02 +0100

    add physics resources and a new physics module function

--- module_interfaces/ntsh_physics_module_interface.h
+++ module_interfaces/ntsh_physics_module_interface.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "ntsh_module_interface.h"
+#include "../resources/ntsh_resources_physics.h"
 
 class NutshellPhysicsModuleInterface : public NutshellModuleInterface {
 public:
@@ -10,4 +11,7 @@ public:
 	virtual void init() = 0;
 	virtual void update(double dt) = 0;
 	virtual void destroy() = 0;
+
+	// Returns true if the two shapes are intersecting with each other, else, returns false
+	virtual bool intersect(NtshColliderShape* shape1, NtshColliderShape* shape2) = 0;
 };
---
+++ resources/ntsh_resources_physics.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <array>
+
+namespace Ntsh {
+
+// Collision
+enum class ColliderShapeType {
+	Sphere,
+	AABB,
+	Capsule,
+	Unknown
+};
+
+struct ColliderShape {
+	ColliderShape(const ColliderShapeType shapeType): type(shapeType) {}
+
+	ColliderShapeType type = ColliderShapeType::Unknown;
+};
+
+struct ColliderSphere: public ColliderShape {
+	ColliderSphere(): ColliderShape(ColliderShapeType::Sphere) {}
+
+	std::array<float, 3> center = { 0.0f, 0.0f, 0.0f };
+	float radius = 0.0f;
+};
+
+struct ColliderAABB: public ColliderShape {
+	ColliderAABB(): ColliderShape(ColliderShapeType::AABB) {}
+
+	std::array<float, 3> min = { 0.0f, 0.0f, 0.0f };
+	std::array<float, 3> max = { 0.0f, 0.0f, 0.0f };
+};
+
+struct ColliderCapsule: public ColliderShape {
+	ColliderCapsule(): ColliderShape(ColliderShapeType::Capsule) {}
+
+	std::array<float, 3> center1 = { 0.0f, 0.0f, 0.0f };
+	float radius1 = 0.0f;
+	std::array<float, 3> center2 = { 0.0f, 0.0f, 0.0f };
+	float radius2 = 0.0f;
+};
+
+}
