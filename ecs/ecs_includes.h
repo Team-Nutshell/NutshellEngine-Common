7b4e75f5b69e8c9faa843fa19667a3eb5c692093
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-15 21:44:01 +0100

    add ecs components to the eader

--- ecs/ecs_includes.h
+++ ecs/ecs_includes.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "ecs.h"
+
 #include "components/transform.h"
+
 #include "components/renderable.h"
-#include "components/collidable.h"
+#include "components/camera.h"
+
+#include "components/spherecollidable.h"
+#include "components/aabbcollidable.h"
+#include "components/capsulecollidable.h"
