9c82bfc0875b2e12079abee5a01f8b498ea2361e
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-16 19:49:19 +0100

    pass entityID to script

--- resources/ntshengn_resources_scripting.h
+++ resources/ntshengn_resources_scripting.h
@@ -11,6 +11,7 @@ namespace NtshEngn {
 	class WindowModuleInterface;
 	class AudioModuleInterface;
 	class ECS;
+	using Entity = int32_t;
 
 	// Script
 	struct Script {
@@ -21,17 +22,18 @@ namespace NtshEngn {
 		virtual void destroy() = 0;
 
 		std::unique_ptr<Script> clone() { return std::unique_ptr<Script>(clone_impl()); }
+		void setEntityID(Entity entityID) { m_entityID = entityID; }
 		void setModules(GraphicsModuleInterface* graphicsModule, PhysicsModuleInterface* physicsModule, WindowModuleInterface* windowModule, AudioModuleInterface* audioModule) {
 			m_graphicsModule = graphicsModule;
 			m_physicsModule = physicsModule;
 			m_windowModule = windowModule;
 			m_audioModule = audioModule;
 		}
-		void setECS(ECS* ecs) {
-			m_ecs = ecs;
-		}
+		void setECS(ECS* ecs) { m_ecs = ecs; }
 	protected:
 		virtual Script* clone_impl() = 0;
+
+		Entity m_entityID = 0;
 
 		GraphicsModuleInterface* m_graphicsModule = nullptr;
 		PhysicsModuleInterface* m_physicsModule = nullptr;
