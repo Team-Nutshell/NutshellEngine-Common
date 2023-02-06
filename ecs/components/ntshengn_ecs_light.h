18c66cba592c1012cda4814575251b15bcfec273
ntsh-oni <teamnutshell.dev@gmail.com>
2023-02-06 13:51:31 +0100

    default values for Light component

--- ecs/components/ntshengn_ecs_light.h
+++ ecs/components/ntshengn_ecs_light.h
@@ -14,8 +14,8 @@ namespace NtshEngn {
 	// Point Light : position = Transform.position
 	// Spot Light : position = Transform.position, direction = Transform.rotation, cutoffs = Transform.scale.xy
 	struct Light {
-		LightType type;
-		std::array<float, 3> color;
+		LightType type = LightType::Unknown;
+		std::array<float, 3> color = { 0.0f, 0.0f, 0.0f };
 	};
 
 }
