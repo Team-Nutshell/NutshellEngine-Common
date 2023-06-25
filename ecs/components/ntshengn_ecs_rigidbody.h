f19bc92c3206347d98a08c36c4cccbce8457ae3a
ntsh-oni <teamnutshell.dev@gmail.com>
2023-06-25 16:36:49 +0200

    add a parameter to rigidbody to get affected by the physics module constant force

--- ecs/components/ntshengn_ecs_rigidbody.h
+++ ecs/components/ntshengn_ecs_rigidbody.h
@@ -5,11 +5,12 @@ namespace NtshEngn {
 
 	struct Rigidbody {
 		bool isStatic = false;
+		bool isAffectedByConstants = true;
 		std::array<float, 3> force = { 0.0f, 0.0f, 0.0f };
-		float mass = 1.0f;
-		float restitution = 0.0f;
 		std::array<float, 3> torque = { 0.0f, 0.0f, 0.0f };
+		float mass = 1.0f;
 		float inertia = 1.0f;
+		float restitution = 0.0f;
 	};
 
 }
