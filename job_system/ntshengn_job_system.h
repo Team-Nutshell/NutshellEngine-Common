4c3c20d33698787d16b34e83d1824d57fc5e7b57
ntsh-oni <teamnutshell.dev@gmail.com>
2023-06-28 20:54:23 +0200

    implement job system in header file

--- job_system/ntshengn_job_system.h
+++ job_system/ntshengn_job_system.h
@@ -23,15 +23,87 @@ namespace NtshEngn {
 
 	class JobSystem {
 	public:
-		JobSystem();
-		~JobSystem();
+		JobSystem() {
+			m_numThreads = std::max(1u, std::thread::hardware_concurrency());
+			m_sharedData.currentJobs.store(0);
+			m_sharedData.running.store(true);
 
-		void execute(const std::function<void()>& job);
-		void dispatch(uint32_t jobCount, uint32_t jobsPerWorker, const std::function<void(JobDispatchArguments)>& job);
-		bool isBusy();
-		void wait();
+			for (uint32_t threadID = 0; threadID < m_numThreads; threadID++) {
+				std::thread worker([&m_sharedData = m_sharedData] () {
+					std::function<void()> job;
 
-		uint32_t getNumThreads() const;
+					while (m_sharedData.running.load()) {
+						if (m_sharedData.jobQueue.pop_front(job)) {
+							job();
+							m_sharedData.currentJobs.fetch_sub(1);
+						}
+						else {
+							std::unique_lock<std::mutex> lock(m_sharedData.wakeMutex);
+							m_sharedData.wakeCondition.wait(lock);
+						}
+					}
+				});
+
+				worker.detach();
+			}
+		}
+
+		~JobSystem() {
+			m_sharedData.running.store(false);
+			for (uint32_t threadID = 0; threadID < m_numThreads; threadID++) {
+				m_sharedData.wakeCondition.notify_one();
+			}
+		}
+
+		void execute(const std::function<void()>& job) {
+			m_sharedData.jobQueue.push_back(job);
+			
+			m_sharedData.currentJobs.fetch_add(1);
+
+			m_sharedData.wakeCondition.notify_one();
+		}
+
+		void dispatch(uint32_t jobCount, uint32_t jobsPerWorker, const std::function<void(JobDispatchArguments)>& job) {
+			if (jobCount == 0 || jobsPerWorker == 0) {
+				return;
+			}
+
+			const uint32_t workerCount = (jobCount + jobsPerWorker - 1) / jobsPerWorker;
+
+			m_sharedData.currentJobs.fetch_add(workerCount);
+
+			for (uint32_t workerIndex = 0; workerIndex < workerCount; workerIndex++) {
+				const uint32_t workerJobOffset = workerIndex * jobsPerWorker;
+				const uint32_t workerJobEnd = std::min(workerJobOffset + jobsPerWorker, jobCount);
+
+				std::function<void()> dispatchJobForWorker = [workerJobOffset, workerJobEnd, workerIndex, job] () {
+					JobDispatchArguments dispatchArguments;
+					dispatchArguments.workerIndex = workerIndex;
+
+					for (uint32_t jobIndex = workerJobOffset; jobIndex < workerJobEnd; jobIndex++) {
+						dispatchArguments.jobIndex = jobIndex;
+
+						job(dispatchArguments);
+					}
+				};
+
+				m_sharedData.jobQueue.push_back(dispatchJobForWorker);
+
+				m_sharedData.wakeCondition.notify_one();
+			}
+		}
+
+		bool isBusy() {
+			return m_sharedData.currentJobs.load() != 0;
+		}
+		
+		void wait() {
+			while (isBusy()) {}
+		}
+
+		uint32_t getNumThreads() const {
+			return m_numThreads;
+		}
 
 	private:
 		uint32_t m_numThreads = 0;
