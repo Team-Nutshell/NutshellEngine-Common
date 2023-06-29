5110a04be1195378b2cb35b87777b1420920d914
ntsh-oni <teamnutshell.dev@gmail.com>
2023-06-29 22:39:43 +0200

    remove torque and inertia from Rigidbody component

--- ecs/components/ntshengn_ecs_rigidbody.h
+++ ecs/components/ntshengn_ecs_rigidbody.h
@@ -7,9 +7,7 @@ namespace NtshEngn {
 		bool isStatic = false;
 		bool isAffectedByConstants = true;
 		std::array<float, 3> force = { 0.0f, 0.0f, 0.0f };
-		std::array<float, 3> torque = { 0.0f, 0.0f, 0.0f };
 		float mass = 1.0f;
-		float inertia = 1.0f;
 		float restitution = 0.0f;
 		float staticFriction = 0.0f;
 		float dynamicFriction = 0.0f;
