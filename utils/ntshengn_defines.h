793408deef4d767ac859f28a4ca6c926c326b1b4
ntsh-oni <teamnutshell.dev@gmail.com>
2023-07-21 17:40:04 +0200

    add a new NTSHENGN_BREAKPOINT macro to add breakpoints with code and use it in asserts

--- utils/ntshengn_defines.h
+++ utils/ntshengn_defines.h
@@ -1,6 +1,24 @@
 #pragma once
 #include <iostream>
 
+#if defined(NTSHENGN_DEBUG)
+#if defined(NTSHENGN_COMPILER_MSVC)
+#define NTSHENGN_BREAKPOINT() \
+	do { \
+		__debugbreak(); \
+	} while(0)
+#elif defined(NTSHENGN_COMPILER_GCC) || defined(NTSHENGN_COMPILER_CLANG)
+#define NTSHENGN_BREAKPOINT() \
+	do { \
+		__builtin_trap(); \
+	} while(0)
+#endif
+#else
+#define NTSHENGN_BREAKPOINT() \
+	do { \
+	} while(0)
+#endif
+
 #define NTSHENGN_UNUSED(expression) \
 	do { \
 		(void)(expression); \
@@ -11,6 +29,7 @@
 	do { \
 		if (!(expression)) { \
 			std::cerr << "\33[1m\33[31mASSERTION FAILED\33[39m\33[0m: " << #expression << ".\nLine \33[1m" << __LINE__ << "\33[0m in file \33[1m" << __FILE__ << "\33[0m." << std::endl; \
+			NTSHENGN_BREAKPOINT(); \
 			exit(1); \
 		} \
 	} while(0)
