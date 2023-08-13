a70a933390acf61bac7d732723e40b316be80ace
ntsh-oni <teamnutshell.dev@gmail.com>
2023-08-13 11:38:54 +0200

    add ui line and rectangle

--- module_interfaces/ntshengn_graphics_module_interface.h
+++ module_interfaces/ntshengn_graphics_module_interface.h
@@ -23,6 +23,10 @@ namespace NtshEngn {
 
 		// Draws a text on the UI with the font in the fontID parameter using the position on screen and color
 		virtual void drawUIText(FontID fontID, const std::string& text, const Math::vec2& position, const Math::vec4& color) = 0;
+		// Draws a line on the UI according to its start and end points and its color
+		virtual void drawUILine(const Math::vec2& start, const Math::vec2& end, const Math::vec4& color) = 0;
+		// Draws a rectangle on the UI according to its position, its size (width and height) and its color
+		virtual void drawUIRectangle(const Math::vec2& position, const Math::vec2& size, const Math::vec4& color) = 0;
 	};
 
 }
