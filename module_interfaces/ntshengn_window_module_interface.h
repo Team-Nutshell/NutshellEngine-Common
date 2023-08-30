f0eb1d45e920938585c1796e2577264d651ef794
ntsh-oni <teamnutshell.dev@gmail.com>
2023-08-30 19:37:55 +0200

    change window functions names

--- module_interfaces/ntshengn_window_module_interface.h
+++ module_interfaces/ntshengn_window_module_interface.h
@@ -18,11 +18,11 @@ namespace NtshEngn {
 		virtual void destroy() = 0;
 
 		// Opens a new window and returns a unique identifier
-		virtual WindowID open(int width, int height, const std::string& title) = 0;
+		virtual WindowID openWindow(int width, int height, const std::string& title) = 0;
 		// Returns true if the window with identifier windowID is open, else, returns false
-		virtual bool isOpen(WindowID windowID) = 0;
+		virtual bool isWindowOpen(WindowID windowID) = 0;
 		// Flags the window to be closed at the next update
-		virtual void close(WindowID windowID) = 0;
+		virtual void closeWindow(WindowID windowID) = 0;
 
 		// Returns the main window identifier
 		virtual WindowID getMainWindowID() = 0;
@@ -30,41 +30,41 @@ namespace NtshEngn {
 		virtual uint64_t windowCount() = 0;
 
 		// Changes the size of the window with identifier windowID to width and height
-		virtual void setSize(WindowID windowID, int width, int height) = 0;
+		virtual void setWindowSize(WindowID windowID, int width, int height) = 0;
 		// Returns the width of the window with identifier windowID
-		virtual int getWidth(WindowID windowID) = 0;
+		virtual int getWindowWidth(WindowID windowID) = 0;
 		// Returns the height of the window with identifier windowID
-		virtual int getHeight(WindowID windowID) = 0;
+		virtual int getWindowHeight(WindowID windowID) = 0;
 
 		// Changes the position of the window with identifier windowID to x and y
-		virtual void setPosition(WindowID windowID, int x, int y) = 0;
+		virtual void setWindowPosition(WindowID windowID, int x, int y) = 0;
 		// Returns the horizontal position of the window with identifier windowID
-		virtual int getPositionX(WindowID windowID) = 0;
+		virtual int getWindowPositionX(WindowID windowID) = 0;
 		// Returns the vertical position of the window with identifier windowID
-		virtual int getPositionY(WindowID windowID) = 0;
+		virtual int getWindowPositionY(WindowID windowID) = 0;
 
 		// If the fullscreen parameter is true, puts the window with identifier windowID in fullscreen, else, puts the window with identifier windowID in windowed
-		virtual void setFullscreen(WindowID windowID, bool fullscreen) = 0;
+		virtual void setWindowFullscreen(WindowID windowID, bool fullscreen) = 0;
 		// Returns true if the window with identifier windowID is in fullscreen mode, else, returns false
-		virtual bool isFullscreen(WindowID windowID) = 0;
+		virtual bool isWindowFullscreen(WindowID windowID) = 0;
 
 		// If the borderless parameter is true, puts the window with identifier windowID in borderless, else, adds borders to the window with identifier windowID
-		virtual void setBorderless(WindowID windowID, bool borderless) = 0;
+		virtual void setWindowBorderless(WindowID windowID, bool borderless) = 0;
 		// Returns true if the window with identifier windowID is in borderless mode, else, returns false
-		virtual bool isBorderless(WindowID windowID) = 0;
+		virtual bool isWindowBorderless(WindowID windowID) = 0;
 
 		// If the resizable parameter is true, makes the window with identifier windowID resizable, else, makes the window with identifier windowID not resizable
-		virtual void setResizable(WindowID windowID, bool resizable) = 0;
+		virtual void setWindowResizable(WindowID windowID, bool resizable) = 0;
 		// Returns true if the window with identifier windowID is resizable, else, returns false
-		virtual bool isResizable(WindowID windowID) = 0;
+		virtual bool isWindowResizable(WindowID windowID) = 0;
 
 		// Polls events of the windows
 		virtual void pollEvents() = 0;
 
 		// Sets the title of the window with identifier windowID
-		virtual void setTitle(WindowID windowID, const std::string& title) = 0;
+		virtual void setWindowTitle(WindowID windowID, const std::string& title) = 0;
 		// Sets the icon of the window with identifier windowID
-		virtual void setIcon(WindowID windowID, const Image& image) = 0;
+		virtual void setWindowIcon(WindowID windowID, const Image& image) = 0;
 
 		// Returns the state of the keyboard key. None if the input is neutral, Pressed the first frame it is being pressed, Held from the second frame it is pressed, Released the frame it is being released
 		virtual InputState getKeyState(WindowID windowID, InputKeyboardKey key) = 0;
