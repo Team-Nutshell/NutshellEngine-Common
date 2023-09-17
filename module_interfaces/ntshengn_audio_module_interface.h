641556454219b7d423ae3814462ec0f12c7805f7
ntsh-oni <teamnutshell.dev@gmail.com>
2023-09-17 14:44:59 +0200

    rework audio

--- module_interfaces/ntshengn_audio_module_interface.h
+++ module_interfaces/ntshengn_audio_module_interface.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ntshengn_system_module_interface.h"
 #include "../resources/ntshengn_resources_audio.h"
+#include "../utils/ntshengn_utils_math.h"
 
 namespace NtshEngn {
 
@@ -17,25 +18,34 @@ namespace NtshEngn {
 		// Loads the sound described in the sound parameter in the internal format and returns a unique identifier
 		virtual SoundID load(const Sound& sound) = 0;
 
-		// Plays the sound with identifier soundID, if the sound is paused, it is resumed
-		virtual void play(SoundID soundID) = 0;
-		// Pauses the sound with identifier soundID
-		virtual void pause(SoundID soundID) = 0;
-		// Stops the sound with identifier soundID
-		virtual void stop(SoundID soundID) = 0;
+		// Plays the sound with identifier soundID at a certain gain and pitch and returns a unique identifier
+		virtual SoundSourceID playSound(SoundID soundID, float gain, float pitch) = 0;
+		// Plays the sound with identifier soundID at a specific position and at a certain gain and pitch and returns a unique identifier
+		virtual SoundSourceID playSoundAtPosition(SoundSourceID soundSourceID, const Math::vec3& position, float gain, float pitch) = 0;
+		// Pauses the sound with identifier soundSourceID
+		virtual void resumeSoundSource(SoundSourceID soundSourceID) = 0;
+		// Pauses the sound with identifier soundSourceID
+		virtual void pauseSoundSource(SoundSourceID soundSourceID) = 0;
+		// Stops the sound with identifier soundSourceID
+		virtual void stopSoundSource(SoundSourceID soundSourceID) = 0;
 
-		// Returns true if the sound with identifier soundID is currently playing, else, returns false
-		virtual bool isPlaying(SoundID soundID) = 0;
+		// Returns the state of the sound source
+		virtual SoundSourceState getSoundSourceState(SoundSourceID soundSourceID) = 0;
+		// Returns true if the sound with identifier soundID has any sound source currently playing, else, returns false
+		virtual bool isSoundPlaying(SoundID soundID) = 0;
 
-		// Sets the gain of the sound with identifier soundID
-		virtual void setGain(SoundID soundID, float newGain) = 0;
-		// Gets the gain of the sound with identifier soundID
-		virtual float getGain(SoundID soundID) = 0;
+		// Sets the gain of the sound source with identifier soundSourceID
+		virtual void setSoundSourceGain(SoundSourceID soundSourceID, float newGain) = 0;
+		// Gets the gain of the sound source with identifier soundSourceID
+		virtual float getSoundSourceGain(SoundSourceID soundSourceID) = 0;
 
-		// Sets the pitch of the sound with identifier soundID
-		virtual void setPitch(SoundID soundID, float newPitch) = 0;
-		// Gets the pitch of the sound with identifier soundID
-		virtual float getPitch(SoundID soundID) = 0;
+		// Sets the pitch of the sound source with identifier soundSourceID
+		virtual void setSoundSourcePitch(SoundSourceID soundSourceID, float newPitch) = 0;
+		// Gets the pitch of the sound source with identifier soundSourceID
+		virtual float getSoundSourcePitch(SoundSourceID soundSourceID) = 0;
+
+		// Sets the entity that will listen to sounds played with playAtPosition
+		virtual void setSoundListenerEntity(Entity entity) = 0;
 	};
 
 }
--- resources/ntshengn_resources_audio.h
+++ resources/ntshengn_resources_audio.h
@@ -9,6 +9,9 @@ namespace NtshEngn {
 	typedef uint32_t SoundID;
 	#define NTSHENGN_SOUND_UNKNOWN 0xFFFFFFFF
 
+	typedef uint32_t SoundSourceID;
+	#define NTSHENGN_SOUND_SOURCE_UNKNOWN 0xFFFFFFFF
+
 	struct Sound {
 		// Number of channels (1 = Mono, 2 = Stereo, ...)
 		uint8_t channels = 0;
@@ -24,6 +27,12 @@ namespace NtshEngn {
 
 		// Data
 		std::vector<uint8_t> data;
+	};
+
+	enum class SoundSourceState {
+		Playing,
+		Paused,
+		Stopped
 	};
 
 }
