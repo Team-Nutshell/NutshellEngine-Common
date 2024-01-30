0cecf4d71188b4a77a76af57608fc43bf76c7736
ntsh-oni <teamnutshell.dev@gmail.com>
2024-01-30 16:47:29 +0100

    add onCollisionEnter, onCollisionStill and onCollisionExit to ScriptBase

--- resources/ntshengn_resources_scripting.h
+++ resources/ntshengn_resources_scripting.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "../utils/ntshengn_utils_math.h"
+#include <vector>
 #include <memory>
 
 #define NTSHENGN_SCRIPT(scriptName) \
@@ -7,11 +9,32 @@
 
 namespace NtshEngn {
 
+	// Collision info
+	struct CollisionInfo {
+		using Entity = uint32_t;
+
+		Entity otherEntity = 0xFFFFFFFF;
+		Math::vec3 normal;
+		float depth = 0.0f;
+		std::vector<Math::vec3> relativePoints;
+	};
+
 	// Script
 	struct ScriptBase {
 		virtual ~ScriptBase() = default;
 
 		std::unique_ptr<ScriptBase> clone() { return std::unique_ptr<ScriptBase>(cloneImpl()); }
+
+	public:
+		virtual void onCollisionEnter(CollisionInfo collisionInfo) {
+			NTSHENGN_UNUSED(collisionInfo);
+		}
+		virtual void onCollisionStill(CollisionInfo collisionInfo) {
+			NTSHENGN_UNUSED(collisionInfo);
+		}
+		virtual void onCollisionExit(CollisionInfo collisionInfo) {
+			NTSHENGN_UNUSED(collisionInfo);
+		}
 
 	protected:
 		virtual ScriptBase* cloneImpl() = 0;
