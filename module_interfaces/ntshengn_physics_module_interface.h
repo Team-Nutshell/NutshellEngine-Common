0b9163668e8a5e8ffc939ffbc2dffa4a8c1b74e9
ntsh-oni <teamnutshell.dev@gmail.com>
2024-03-19 18:29:34 +0100

    minor comment fix

--- module_interfaces/ntshengn_physics_module_interface.h
+++ module_interfaces/ntshengn_physics_module_interface.h
@@ -19,7 +19,7 @@ namespace NtshEngn {
 		// Returns an IntersectionInformation structure containing information about the intersection
 		virtual IntersectionInformation intersect(const ColliderShape* shape1, const ColliderShape* shape2) = 0;
 		
-		// Returns a RaycastInformation structure containinng informationn about the raycast
+		// Returns a RaycastInformation structure containing information about the raycast
 		virtual RaycastInformation raycast(const Math::vec3& rayOrigin, const Math::vec3& rayDirection, float tMin, float tMax, const ColliderShape* shape) = 0;
 		// Returns a list of RaycastInformation structures containing information about the hit entities
 		virtual std::vector<std::pair<Entity, RaycastInformation>> raycastAll(const Math::vec3& rayOrigin, const Math::vec3& rayDirection, float tMin, float tMax) = 0;
