45c75af6800790e8daa19807ed7fa78dd67f0c49
ntsh-oni <teamnutshell.dev@gmail.com>
2025-08-15 20:20:43 +0200

    add functions related to achievements

--- modules/ntshengn_platform_module_interface.h
+++ modules/ntshengn_platform_module_interface.h
@@ -17,9 +17,16 @@ namespace NtshEngn {
 		// Returns the list of the user's friends on the platform
 		virtual std::vector<PlatformUserInfo> getUserFriendsInfo() = 0;
 
+		// Unlocks achievement with identifier achievementID
+		virtual void unlockAchievement(const std::string& achievementID) = 0;
+		// Locks achievement with identifier achievementID
+		virtual void lockAchievement(const std::string& achievementID) = 0;
+		// Returns true if the achievement with identifier achievementID has been unlocked by the user, else, returns false
+		virtual bool isAchievementUnlocked(const std::string& achievementID) = 0;
+
 		// Show the platform's overlay
 		virtual void showOverlay() = 0;
-		// Returns true if the platform's overlay is visible
+		// Returns true if the platform's overlay is visible, else, returns false
 		virtual bool isOverlayVisible() = 0;
 	};
 
